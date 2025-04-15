f733df13ed52f73a9dfb87a3cd0f3420d733ccae
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-15 11:01:23 +0300

    Refactor Api class methods for improved readability and consistency

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,20 +1,38 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-Future<dynamic> get({required String url}) async{
-    http.Response response = await http.get(
+  Future<dynamic> get({required String url}) async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
+  Future<dynamic> post({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    http.Response response = await http.post(
       Uri.parse(url),
+      body: body,
+      headers: headers,
     );
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
-
-} else {
+    } else {
       throw Exception('Failed to load products');
     }
-
-}
-
-
+  }
 }
