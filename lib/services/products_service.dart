e25eb9e8b625fc42107cb35750be8414c0ea1cd5
mahmoud abdo <52537759+mahabdo571@users.noreply.github.com>
2025-04-15 09:44:46 +0300

    Implement ProductsService to fetch all products from API

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -1,9 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:mini_store/models/products_model.dart';
 
 class ProductsService {
+  Future<List<Product>> getAllProducts() async {
+    http.Response response = await http.get(
+      Uri.parse('https://fakestoreapi.com/products'),
+    );
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
 
-Future<List<Product>> getAllProducts() async {
- 
- 
-
+      List<Product> products =
+          data.map((product) => Product.fromJson(product)).toList();
+      return products;
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
 }
